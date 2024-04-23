68086c176463c8fdaf7525fafed599172258c87f
Eben Janse van Rensburg <ebenj.van.rensburg@gmail.com>
2024-04-23 10:31:14 +0200

    Unit 2 Lesson 10 complete

---
+++ unit_2/lesson_10/express_views_server/controllers/homeController.js
@@ -0,0 +1,17 @@
+"use strict";
+
+exports.sendReqParam = (req, res) => {
+  let veg = req.params.vegetable;
+  res.send(`This is the page for ${veg}`);
+};
+
+exports.respondWithName = (req, res) => {
+  // Assign a local variable to a request parameter.
+  let paramsName = req.params.myName;
+  // Pass a local variable to a rendered view.
+  res.render("index", { name: paramsName });
+};
+
+// exports.respondWithName = (req, res) => {
+//   res.render("index");
+// };

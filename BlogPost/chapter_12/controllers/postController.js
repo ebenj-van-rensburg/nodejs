c3f99edad1e8df051c2907002b15df8921841bcb
Eben Janse van Rensburg <ebenj.van.rensburg@gmail.com>
2024-04-25 00:46:30 +0200

    Tutorial 1 Chapter 12 complete

---
+++ BlogPost/chapter_12/controllers/postController.js
@@ -0,0 +1,28 @@
+const BlogPost = require('../models/BlogPost.js');
+const path = require('path');
+
+module.exports = {
+  getPost: async (req, res) => {
+    const blogpost = await BlogPost.findById(req.params.id);
+    // console.log(blogpost);
+    res.render('post', {
+      blogpost: blogpost
+    });
+  },
+  newPost: (req, res) => {
+    if (req.session.userId) {
+      return res.render("create");
+    }
+    res.redirect('/auth/login')
+  },
+  storePost: async (req, res) => {
+    const image = req.files.image;
+    image.mv(path.resolve(__dirname, '/public/img', image.name), async (error) => {
+      await BlogPost.create({
+        ...req.body,
+        image: '/img/' + image.name
+      })
+      res.redirect('/');
+    })
+  }
+}
